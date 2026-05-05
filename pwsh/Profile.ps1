657530fa9a5fba6f9c05bd5c96a25ac18009c6cd
vinhvn <vincentn337@gmail.com>
2026-05-05 18:49:11 -0400

    fix: editor config, aliases and autocomplete

--- pwsh/Profile.ps1
+++ pwsh/Profile.ps1
@@ -1,34 +1,57 @@
 $isInteractive = -not [Console]::IsInputRedirected -and -not [Console]::IsOutputRedirected
 
-if ($isInteractive -and (Get-Module -ListAvailable -Name Terminal-Icons)) {
-    Import-Module -Name Terminal-Icons -ErrorAction SilentlyContinue
-}
-
 $dotfilesRoot = Split-Path -Parent $PSScriptRoot
 $themePath = Join-Path $dotfilesRoot "themes\catppuccino.omp.json"
+$env:POSH_GIT_ENABLED = $true
 
-if ($isInteractive -and (Get-Command oh-my-posh -ErrorAction SilentlyContinue) -and (Test-Path -LiteralPath $themePath)) {
-    oh-my-posh init pwsh --config $themePath | Invoke-Expression
-}
-
-if ($isInteractive -and (Get-Module -ListAvailable -Name posh-git)) {
-    Import-Module -Name posh-git -ErrorAction SilentlyContinue
-}
-
-if ($isInteractive -and (Get-Module -ListAvailable -Name PSReadLine)) {
-    Import-Module -Name PSReadLine -ErrorAction SilentlyContinue
-
-    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-    Set-PSReadLineOption -PredictionViewStyle ListView
-    Set-PSReadLineKeyHandler -Key Tab -Function Complete
-    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+function Register-Git {
+  Invoke-Expression -Command "function global:$($args[0]) { git $($args[1]) @args }"
+  $tab = [Scriptblock]::Create("
+    param(`$wordToComplete, `$commandAst, `$cursorPosition)
+    `$cmdline = `$commandAst.ToString().Replace(`"$($args[0])`", `"`")
+    if (`$wordToComplete.Length -ne 0) { `$tail = '' } else { `$tail = ' ' }
+    Expand-GitCommand `"git $($args[1]) `$cmdline`$tail`"
+  ")
+  Register-ArgumentCompleter -CommandName $args[0] -ScriptBlock $tab
 }
 
 if ($isInteractive) {
-    function gfa { & git fetch --all $args }
-    function gpr { & git fetch origin "$($args[0]):$($args[0])" }
+    # Aliases
+    function gfo { & git fetch origin "$($args[0]):$($args[0])" }
+    Register-Git ga "add"
+    Register-Git gb "branch"
+    Register-Git gco "checkout"
+    Register-Git gs "status"
+    Register-Git log "log --oneline --graph --decorate"
+    Register-Git gd "diff"
+    Register-Git gds "diff --staged"
+    Register-Git gcl "clone"
+    Register-Git gf "fetch --all"
     function rmf { Remove-Item -LiteralPath $args[0] -Force }
     function rmr { Remove-Item -LiteralPath $args[0] -Recurse }
     function rmrf { Remove-Item -LiteralPath $args[0] -Recurse -Force }
+
+    # Modules
+    if (Get-Module -ListAvailable -Name Terminal-Icons) {
+        Import-Module -Name Terminal-Icons
+    }
+
+    if (Get-Module -ListAvailable -Name PSReadLine) {
+        Import-Module -Name PSReadLine
+
+        Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+        Set-PSReadLineOption -PredictionViewStyle ListView
+        Set-PSReadLineKeyHandler -Key Tab -Function Complete
+        Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+        Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+    }
+
+    # oh-my-posh
+    if ((Get-Command oh-my-posh -ErrorAction SilentlyContinue) -and (Test-Path -LiteralPath $themePath)) {
+        oh-my-posh init pwsh --config $themePath | Invoke-Expression
+    }
+
+    if (Get-Module -ListAvailable -Name posh-git) {
+        Import-Module -Name posh-git
+    }
 }
