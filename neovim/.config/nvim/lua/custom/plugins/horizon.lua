036c6ceafd17d90f7b6a01fa36c6f18abcac6d27
vincendiary <vincentn337@gmail.com>
2026-06-28 10:33:42 -0400

    feat: dynamic neovim theme

--- neovim/.config/nvim/lua/custom/plugins/horizon.lua
+++ neovim/.config/nvim/lua/custom/plugins/horizon.lua
@@ -1,6 +1,16 @@
 vim.pack.add { 'https://github.com/lancewilhelm/horizon-extended.nvim' }
-require("horizon-extended").setup({
-  style = "ray",
-})
-vim.cmd.colorscheme 'horizon-extended'
 
+local function apply()
+  require("horizon-extended").setup({
+    style = vim.o.background == "light" and "ray" or "neo",
+  })
+  vim.cmd.colorscheme 'horizon-extended'
+end
+
+apply()
+
+-- re-theme when terminal flips dark/light (nvim sets 'background' via OSC 11)
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = "background",
+  callback = apply,
+})
